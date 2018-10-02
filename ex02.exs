5bf12436542e93255a99c653a7000f26d7fb13b1
Zach Banducci <zbducci@gmail.com>
2018-10-02 17:11:33 -0500

    Global counter implemented and tested

--- ex02.exs
+++ ex02.exs
@@ -8,6 +8,14 @@ defmodule Ex02 do
   def next_value(counter_id) do
     Agent.get_and_update(counter_id, fn value -> {value, value + 1} end)
   end
+
+  def new_global_counter(value \\ 0) do
+    Agent.start_link(fn -> value end, name: Counter)
+  end
+
+  def global_next_value do
+    Agent.get_and_update(Counter, fn value -> {value, value + 1} end)
+  end
 end
 
 ExUnit.start()
@@ -69,12 +77,12 @@ defmodule Test do
   that agent into calls to `global_next_value`?
   """
 
-  # test "global counter" do
-  #   Ex02.new_global_counter
-  #   assert Ex02.global_next_value == 0
-  #   assert Ex02.global_next_value == 1
-  #   assert Ex02.global_next_value == 2
-  # end
+  test "global counter" do
+    Ex02.new_global_counter
+    assert Ex02.global_next_value == 0
+    assert Ex02.global_next_value == 1
+    assert Ex02.global_next_value == 2
+  end
 end
 
 
