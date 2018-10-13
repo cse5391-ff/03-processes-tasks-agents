386874a729221b1a19c93d224ab736bf89d0ce72
Remus Tumac <rtumac@smu.edu>
2018-10-13 12:12:41 -0500

    ex02 - passed 2nd test

--- ex02.exs
+++ ex02.exs
@@ -1,6 +1,15 @@
 
 defmodule Ex02 do
 
+  def new_counter(value \\ 0) do
+    { :ok, agent } = Agent.start_link(fn -> value end)
+    agent
+  end
+  
+  def next_value(count_pid) do
+    Agent.get_and_update(count_pid, &{ &1, &1 + 1 })
+  end
+  
 end
 
 ExUnit.start()
@@ -47,11 +56,11 @@ defmodule Test do
   top of this file to make those tests run.
   """
 
-  # test "higher level API interface" do
-  #   count = Ex02.new_counter(5)
-  #   assert  Ex02.next_value(count) == 5
-  #   assert  Ex02.next_value(count) == 6
-  # end
+  test "higher level API interface" do
+    count = Ex02.new_counter(5)
+    assert  Ex02.next_value(count) == 5
+    assert  Ex02.next_value(count) == 6
+  end
 
   @doc """
   Last (for this exercise), we'll create a global counter by adding
@@ -66,10 +75,5 @@ defmodule Test do
   #   assert Ex02.global_next_value == 1
   #   assert Ex02.global_next_value == 2
   # end
+  
 end
-
-
-
-
-
-
